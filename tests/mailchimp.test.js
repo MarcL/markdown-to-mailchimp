a078357b5715e7cf5a3439d70c1e1269ab8421bd
Marc Littlemore <marc.littlemore@gmail.com>
2020-05-09 21:32:24 +0100

    More Mailchimp tests

--- tests/mailchimp.test.js
+++ tests/mailchimp.test.js
@@ -1,7 +1,27 @@
+const Mailchimp = require('mailchimp-api-v3');
 const mailchimp = require('../src/mailchimp');
 
+jest.mock('mailchimp-api-v3');
+
 describe('Mailchimp', () => {
-    describe('isCampaignSent', () => {
+    let mockMailchimpGet;
+    let mockMailchimpPost;
+    let mockMailchimpPut;
+
+    beforeEach(() => {
+        mockMailchimpGet = jest.fn();
+        mockMailchimpPost = jest.fn();
+        mockMailchimpPut = jest.fn();
+        Mailchimp.mockImplementation(() => {
+            return {
+                get: mockMailchimpGet,
+                post: mockMailchimpPost,
+                put: mockMailchimpPut,
+            };
+        });
+    });
+
+    describe('isCampaignSent()', () => {
         it('should return true if campaign is sent', () => {
             const campaign = {
                 status: 'sent',
@@ -40,6 +60,50 @@ describe('Mailchimp', () => {
             };
 
             expect(mailchimp.isCampaignSent(campaign)).toBe(false);
+        });
+    });
+
+    describe('createCampaign()', () => {
+        it('should create a Mailchimp client with the expected API key', async () => {
+            expect.assertions(1);
+            const expectedApiKey = 'expectedApiKey';
+            const givenOptions = {
+                apiKey: expectedApiKey,
+            };
+
+            await mailchimp.createCampaign(givenOptions);
+
+            expect(Mailchimp).toHaveBeenCalledWith(expectedApiKey);
+        });
+
+        it('should call Mailchimp client post method with expected parameters', async () => {
+            expect.assertions(1);
+            const givenOptions = {
+                type: 'expectedType',
+                listId: 'expectedListId',
+                subject: 'expectedSubject',
+                preview: 'expectedPreview',
+                title: 'expectedTitle',
+                fromName: 'expectedFromName',
+                replyTo: 'expectedReplyTo',
+            };
+
+            await mailchimp.createCampaign(givenOptions);
+
+            expect(mockMailchimpPost).toHaveBeenCalledWith('/campaigns', {
+                type: 'expectedType',
+                recipients: {
+                    list_id: 'expectedListId',
+                },
+                settings: {
+                    subject_line: 'expectedSubject',
+                    preview_text: 'expectedPreview',
+                    title: 'expectedTitle',
+                    from_name: 'expectedFromName',
+                    reply_to: 'expectedReplyTo',
+                    to_name: '*|FNAME|*',
+                },
+            });
         });
     });
 });
